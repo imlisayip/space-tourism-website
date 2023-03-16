359c423d9ad7daf8d7b7ae77dac599b2bd6ffef1
Lisa Yip <imlisayip@gmail.com>
2023-03-16 14:15:24 -0700

    adding functionality to menu button

---
+++ index.js
@@ -0,0 +1,17 @@
+const nav = document.querySelector(".primary-navigation");
+const navToggle = document.querySelector(".mobile-nav-toggle");
+
+navToggle.addEventListener("click", () => {
+  const visibile = nav.getAttribute("data-visible");
+
+  if (visibile === "false") {
+    nav.setAttribute("data-visible", true);
+    navToggle.setAttribute("aria-expanded", true);
+    navToggle.style.backgroundImage = "url('./assets/shared/icon-close.svg')";
+  } else {
+    nav.setAttribute("data-visible", false);
+    navToggle.setAttribute("aria-expanded", false);
+    navToggle.style.backgroundImage =
+      "url('./assets/shared/icon-hamburger.svg')";
+  }
+});
